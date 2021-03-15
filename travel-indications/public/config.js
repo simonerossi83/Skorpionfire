7ef39fa87e0df9467d7dafe078251a4a4dd37549
simonerossi83 <74321537+simonerossi83@users.noreply.github.com>
2021-03-15 11:59:18 +0100

    Delete

--- travel-indications/public/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  clientId : 'D3ADP01Y5SZSCGBXYS4OLI05LACGSIXSZTTLJWEO2434RYZ1',
-  clientSecret : 'J1D255K1DXQ0P11R4FJT5Q5A4VG3EN2LTKI4J0GH1OTGBAE0',
-  url: 'https://api.foursquare.com/v2/venues/explore?near=',
-  openWeatherKey : '2d970d3bb60857551f685a63d4effdb3',
-  weatherUrl: 'https://api.openweathermap.org/data/2.5/weather?q='
-}
-
